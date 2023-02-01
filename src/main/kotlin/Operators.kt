2fadcb68b90137bf93573efa136247d86a586c42
Abhishek Bhattacharjee <abhiias2003@gmail.com>
2023-02-01 12:15:38 +0530

    DAY 2 of Kotlin

---
+++ src/main/kotlin/Operators.kt
@@ -0,0 +1,23 @@
+fun main() {
+
+    val number1 = 12.5
+    val number2 = 3.5
+    var result: Double  // --> Only declaring and not using it
+
+    result = number1 + number2
+    println(result)
+
+    result = number1 - number2
+    println(result)
+
+    result = number1 / number2
+    println(result)
+
+    result = number1 * number2
+    println(result)
+
+    result = number1 % number2
+    print(result)
+
+}
+
---
+++ src/main/kotlin/Variables.kt
@@ -0,0 +1,35 @@
+fun main() {
+
+    var wholeNumber = 10
+    var fractionalNumber = 3.5
+    var sentence = "A new sentence"
+    var condition = false
+    var character = 'a'
+
+    var a: Int = 5
+    var b: String = "Internshala"
+
+    val number = 2345
+    val decimal = 5.6
+    val name = "Internshala"
+    val conditional = true
+    val floatnumber = 36.5f
+
+    wholeNumber += 40
+
+    // decimal += 30.2 --> Gives an error saying that val cannot be reassigned
+
+    // wholeNumber += 10.5 --> Type mismatch (Statically typed language)
+
+    println(wholeNumber)
+    println(fractionalNumber)
+    println(sentence)
+    println(condition)
+
+    println(number)
+    println(decimal)
+    println(name)
+    println(conditional)
+
+
+}
